cc718ec50b011c86d8f8b873cb7dd4345f5afb7e
Tim McCarter <tim@kadenze.com>
2018-01-31 17:41:13 -0800

    adding webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('app.bundle.css')
+  ]
+};
